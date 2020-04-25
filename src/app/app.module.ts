72a826bcbbef411c5430efd4724c83fe02644f1e
Luis Mazariego <mazariego2011@gmail.com>
2020-04-24 23:26:57 -0600

    added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      MenuComponent
+   ],
+   imports: [
+      BrowserModule,
+      RouterModule.forRoot([
+         {path: 'home', component: HomeComponent},
+         { path: '' , redirectTo: '/home', pathMatch: 'full'}
+      ])
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
